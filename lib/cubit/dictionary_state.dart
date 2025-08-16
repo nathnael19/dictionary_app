c2954e058d005e28252f26d6d58ee99955731888
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-08-16 12:50:48 -0400

    creating a dictionary cubit state

--- lib/cubit/dictionary_state.dart
+++ lib/cubit/dictionary_state.dart
@@ -1,1 +1,19 @@
+import 'package:dictionary/model/dictionary_model.dart';
 
+abstract class DictionaryState {}
+
+final class DictionaryFailed extends DictionaryState {
+  final String msg;
+
+  DictionaryFailed({required this.msg});
+}
+
+final class DictionaryInitial extends DictionaryState {}
+
+final class DictionaryLoaded extends DictionaryState {
+  final List<DictionaryModel> dictionary;
+
+  DictionaryLoaded({required this.dictionary});
+}
+
+final class DictionaryLoading extends DictionaryState {}
