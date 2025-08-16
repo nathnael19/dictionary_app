054c86e6aec4f5f93e00b119d37ff9533ad94637
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-08-16 12:28:43 -0400

    creating a model for meaning

---
+++ lib/model/meaning_model.dart
@@ -0,0 +1,22 @@
+import 'package:dictionary/model/definition_model.dart';
+
+class MeaningModel {
+  final String partOfSpeech;
+  final List<DefinitionModel> definitions;
+  final List<String> synonyms;
+  final List<String> antonyms;
+
+  MeaningModel({
+    required this.partOfSpeech,
+    required this.definitions,
+    required this.synonyms,
+    required this.antonyms,
+  });
+
+  factory MeaningModel.fromJson(Map<String, dynamic> json) => MeaningModel(
+    partOfSpeech: json['partOfSpeech'],
+    definitions: json['definitions'],
+    synonyms: json['synonyms'],
+    antonyms: json['antonyms'],
+  );
+}
