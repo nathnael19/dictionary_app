f911cfbf1995e124f998cf4187f6d513af30e49c
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-08-16 12:35:23 -0400

    creating a dictionary model

--- lib/model/dictionary_model.dart
+++ lib/model/dictionary_model.dart
@@ -1,1 +1,28 @@
+import 'package:dictionary/model/license_model.dart';
+import 'package:dictionary/model/meaning_model.dart';
+import 'package:dictionary/model/phonetic_model.dart';
 
+class DictionaryModel {
+  final String word;
+  final List<PhoneticModel> phonetics;
+  final List<MeaningModel> meanings;
+  final LicenseModel license;
+  final List<String> sourceUrls;
+
+  DictionaryModel({
+    required this.word,
+    required this.phonetics,
+    required this.meanings,
+    required this.license,
+    required this.sourceUrls,
+  });
+
+  factory DictionaryModel.fromJson(Map<String, dynamic> json) =>
+      DictionaryModel(
+        word: json['word'],
+        phonetics: json['phonetics'],
+        meanings: json['meanings'],
+        license: json['license'],
+        sourceUrls: json['sourceUrl'],
+      );
+}
